27ecf8e9675530681884aa646206deb8400176c3
Sasha <55252065+Scall01@users.noreply.github.com>
2020-12-28 10:14:26 -0600

    Unit tests for deck

---
+++ testing/test-deck.py
@@ -0,0 +1,23 @@
+"""
+Tests the deck of the bot
+"""
+
+from src.Deck import Deck
+
+
+deck = Deck(1)
+
+assert deck.count == 52
+assert deck.counts[1] == 4
+assert deck.counts[6] == 4
+assert deck.counts[10] == 16
+
+assert deck.chance_of(1) == 4/52
+assert deck.chance_of(5) == 4/52
+assert deck.chance_of(10) == 16/52
+
+draw = deck.draw()[0]
+
+assert deck.count == 51
+assert deck.chance_of(draw) == 3/51
+assert deck.chance_of(draw + 1 if draw != 10 else draw - 1) == 4/52
